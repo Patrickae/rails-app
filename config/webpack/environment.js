673c9e5520ee3ec16aa96ce4bede402242885bd9
Andy <andyepatrick@gmail.com>
2020-08-09 23:39:57 -0400

    udpates

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack');
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jqurey',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
 module.exports = environment
