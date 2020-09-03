baac428a408e77f3de6ae5d09f98b430ec7ef8e8
Andy <andyepatrick@gmail.com>
2020-09-02 20:27:01 -0400

    user updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  def show
+    @articles = @user.articles
+  end
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def gravatar_for(user, options={size: 80})
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    gravatar_img = "https://www.gravatar.com/avatar/#{hash}?s=#{options[:size]}"
+    image_tag(gravatar_img, alt: user.username, class: 'rounded shadow mx-auto d-block mt-4')
+  end
 end
